229c7e6b03a0119d05fcef60156854c222b4d8bc
Jakson Lima <jaksonsneider@gmail.com>
2019-02-09 16:41:10 -0200

    Aula de Public Final Void 09/02/2019 16:41:00

---
+++ src/Final/Final.java
@@ -0,0 +1,30 @@
+package Final;
+
+public class Final {
+	
+	/*Classes e métodos final
+	 * •Palavra chave: final
+	 * •Classe: evita que a classe seja herdada
+	 * public final classSavingsAccount{
+	 * •Método: evita que o método sob seja sobreposto*/
+
+//	public final classSavingsAccount() {
+//		
+//	}
+	
+	/*
+	 * Exemplo -Classe final 
+	 * Suponha que você queira evitar
+	 *  que sejam criadas subclasses de 
+	 *  SavingsAccount
+	 *  public final classSavingsAccount{(...)*/
+	
+	
+	/*
+	 * Exemplo -método final
+	 * Suponha que você não queira que o método Withdraw de
+	 *  SavingsAccountse ja sobreposto
+	 *  @Overridepublicfinal void withdraw(doubleamount) {balance -= amount;}
+	 * */
+}
+
