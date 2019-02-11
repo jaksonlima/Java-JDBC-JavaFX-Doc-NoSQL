4033ec390f083ef9a526ca1e7897dc4fda81e776
Jakson Wilson Bonfim de Lima <jaksonsneider@gmail.com>
2019-02-11 16:53:28 -0200

    Aula List e Polimorfismo 11-02-2019 16:53:00

--- src/Polimorfismo/List/Herança/ListAddPolimorfismo.java
+++ src/Polimorfismo/List/Herança/ListAddPolimorfismo.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ListAdd {
+public class ListAddPolimorfismo {
 
 	public static void main(String[] args) {
 
@@ -23,6 +23,10 @@ public class ListAdd {
 		
 		lista.add(t);
 		lista.add(f);
+		
+		/**PARA TER UMA LISTA VOCE PRECISA CHAMAR ELA COM (LIST - ARRAYLIST)  E ASSIM PASSAR O NOME DA 
+		 * CLASS QUE VOCE PRECISA DE MAS DE 2 FUNCIONARIO ASSIM VOCE CHAMAR O LISTA.ADD(E PASSA O CONSTRUTOR)
+		 * DA CLASS QUE DESEJA*/
 
 	}
 
