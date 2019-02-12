027fdbc44ee532a7dad1a93ece8a8460b208cad1
Jakson Lima <jaksonsneider@gmail.com>
2019-02-11 22:41:23 -0200

    Aula List Polimorfismo 11-02-2019 22:41:00

---
+++ src/ListPolimorfismoHeranca/Produto.java
@@ -0,0 +1,36 @@
+package ListPolimorfismoHeranca;
+
+public class Produto {
+
+	private String nome;
+	private Double preco;
+
+	public Produto() {
+	}
+
+	public Produto(String nome, Double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String EtiquetaPreco() {
+		return nome + " $ " + String.format("%.2f", preco);
+	}
+
+}
