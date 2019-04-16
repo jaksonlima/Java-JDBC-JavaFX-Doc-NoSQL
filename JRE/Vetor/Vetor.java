262a548657b75caacf23cf98c0a3422bc9cfb5a2
Jakson Wilson Bonfim de Lima <jaksonsneider@gmail.com>
2019-04-16 14:09:31 -0300

    Vetor

--- JRE/Vetor/Vetor.java
+++ JRE/Vetor/Vetor.java
@@ -5,18 +5,24 @@ import java.util.Scanner;
 public class Vetor {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		String[] mes = new String[12];
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(i);
+		}
+
+		String[] mes = new String[10];
 		System.out.println(mes[1]);
-		
+
+		System.out.print("Nome completo: ");
 		String[] vetor = sc.nextLine().split(" ");
+		System.out.print("Qual possição: ");
 		int posicao = sc.nextInt();
-		System.out.println(vetor[posicao]);
-		
+		System.out.println("Retorno da possição: " + vetor[posicao]);
+
 		/***
-		Assim fazemos um vetor e chamamos a possição 1 do vetor que tem 12 possiçoes 
-		*/
-		
+		 * Assim fazemos um vetor e chamamos a possição 1 do vetor que tem 12 possiçoes
+		 */
+
 		sc.close();
 	}
 
